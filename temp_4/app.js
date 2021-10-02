366c20517e96ed3cf4a859964e776ab8610524f1
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-10-02 18:43:28 +0300

    add: html skeleton

---
+++ temp_4/app.js
@@ -0,0 +1,19 @@
+const accordion = (elem) => {
+    document.addEventListener('click', (e) => {
+        const target = e.target
+        if (!target.matches(elem + ' .a-btn')) return
+        else {
+            if (!target.parentElement.classList.contains('active')) {
+                let elementList = document.querySelectorAll(elem + ' .a-container')
+                elementList.forEach((e) => {
+                    e.classList.remove('active')
+                });
+                target.parentElement.classList.add('active')
+            } else {
+                target.parentElement.classList.remove('active')
+            }
+        }
+    })
+}
+
+accordion('.accordion')
