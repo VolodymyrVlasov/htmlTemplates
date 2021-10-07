037faf4c9bde7269071ee967c69ffcb6fe34df76
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-10-08 01:13:40 +0300

    add custom input range

--- temp_4/app.js
+++ temp_4/app.js
@@ -1,3 +1,10 @@
+//@ts-check
+'use strict'
+
+let a = 10
+a = "fe"
+
+
 const accordion = (elem) => {
     document.addEventListener('click', (e) => {
         const target = e.target
@@ -18,3 +25,34 @@ const accordion = (elem) => {
 
 accordion('.accordion')
 
+const map = (x, in_min, in_max, out_min, out_max) => {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+}
+
+
+const customRange = (id, thumbSize) => {
+    const inputCnt = document.getElementById(id)
+    let inputNode = inputCnt.lastElementChild
+    const labelNode = inputCnt.firstElementChild
+    const thumbHalfSize = thumbSize * 0.5
+    const from = Number(inputNode.min)
+    const to = Number(inputNode.max)
+    const inputWidth = Math.floor(inputNode.offsetWidth)
+
+    inputCnt.addEventListener("input", () => {
+        const value = Number(inputCnt.lastElementChild.value)
+        const labelCenterX = labelNode.offsetWidth * 0.5
+        labelNode.innerHTML = String(value)
+        let gradientPosition = map(value, from, to, 0, 100)
+        let labelPosition = map(value, from, to, 0, inputWidth)
+        labelPosition = (value > to * 0.5) ?
+            labelPosition - map(value, to * 0.5, to, labelCenterX, thumbSize + 1)
+            :
+            labelPosition - map(value, from, to * 0.5, labelCenterX - thumbHalfSize, labelCenterX)
+        inputNode.style.background = `linear-gradient(90deg, var(--color-theme) ${gradientPosition}%, var(--color-light-gray) ${gradientPosition}%)`
+        labelNode.style.transform = `translateX(${labelPosition}px)`
+    })
+}
+
+customRange("input-height", 32)
+customRange("input-width", 32)
