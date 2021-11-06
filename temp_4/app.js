7c4f131de56ec499369606b26969fb48e3fb578a
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-11-06 12:12:38 +0200

    add: calculator init logic

--- temp_4/app.js
+++ temp_4/app.js
@@ -1,3 +1,12 @@
+const calcRequest = {
+    width: 40,
+    height: 60,
+    amount: 1,
+    material: "RitramaMatte",
+    quality: "1440p",
+    price: 210
+}
+
 const accordion = (elem) => {
     document.addEventListener('click', (e) => {
         const target = e.target
@@ -22,7 +31,6 @@ const map = (x, in_min, in_max, out_min, out_max) => {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 }
 
-
 const customRange = (id, thumbSize) => {
     const inputCnt = document.getElementById(id)
     let inputNode = inputCnt.lastElementChild
@@ -32,7 +40,7 @@ const customRange = (id, thumbSize) => {
     const to = Number(inputNode.max)
     const inputWidth = Math.floor(inputNode.offsetWidth)
 
-    inputCnt.addEventListener("input", () => {
+    const changeRangeUI = () => {
         const value = Number(inputCnt.lastElementChild.value)
         const labelCenterX = labelNode.offsetWidth * 0.5
         labelNode.innerHTML = String(value)
@@ -44,8 +52,67 @@ const customRange = (id, thumbSize) => {
             labelPosition - map(value, from, to * 0.5, labelCenterX - thumbHalfSize, labelCenterX)
         inputNode.style.background = `linear-gradient(90deg, var(--color-theme) ${gradientPosition}%, var(--color-light-gray) ${gradientPosition}%)`
         labelNode.style.transform = `translateX(${labelPosition}px)`
-    })
+    }
+
+    inputCnt.addEventListener("input", () => changeRangeUI())
 }
+
+const price = {
+    RitramaMatte: {
+        "720p": [210, 190, 160],
+        "1440p": [260, 210, 190]
+    },
+    RitramaGlossy: {
+        "720p": [210, 190, 160],
+        "1440p": [260, 210, 190]
+    },
+    RitramaBlackout: {
+        "720p": [210, 190, 160],
+        "1440p": [260, 210, 190]
+    },
+    RitramaTransparent: {
+        "720p": [210, 190, 160],
+        "1440p": [260, 210, 190]
+    }
+}
+
+const calculatePrice = () => {
+    const outNode = document.getElementById("price")
+    let sq = (calcRequest.width * calcRequest.height) / 10000
+    console.table(price[calcRequest["material"]][[calcRequest["quality"]]][0], sq)
+    calcRequest.price = sq * price[calcRequest["material"]][[calcRequest["quality"]]][0]
+    outNode.innerText = calcRequest.price
+}
+
+const calculateTime = () => {
+    console.log()
+}
+
+document.getElementById("calculator").addEventListener("change", (e) => {
+    const target = e.target
+
+    switch (target.id) {
+        case "input_range-height":
+            calcRequest.height = Number(target.value)
+            break
+        case "input_range-width":
+            calcRequest.width = Number(target.value)
+            break
+        case "amount":
+            calcRequest.amount = Number(target.value)
+            break
+        case "material":
+            calcRequest.material = target.value
+            break
+        case "quality":
+            calcRequest.quality = target.value
+            break
+    }
+    calculatePrice()
+    calculateTime()
+})
 
 customRange("input-height", 32)
 customRange("input-width", 32)
+
+
